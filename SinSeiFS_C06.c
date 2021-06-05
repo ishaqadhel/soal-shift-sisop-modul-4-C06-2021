4366f75b4781eeb28ad64bfd8815c63d126e8d14
ishaqadhel <ishaq.adhel@gmail.com>
2021-06-05 21:53:48 +0700

    feat(soal1): integrate atbashCipher to folder AtoZ_

--- SinSeiFS_C06.c
+++ SinSeiFS_C06.c
@@ -7,16 +7,52 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
 static const char *dirpath = "/home/namdoshaq/Downloads";
 
+void atbashCipher(char *nameFile, int sizeOfNameFile, int isEncrypt)
+{
+    if (!strcmp(nameFile, ".") || !strcmp(nameFile, "..") || (!strchr(nameFile, '/') && !isEncrypt))
+        return;
+    int startIndex = -1, endOfIndex = -1;
+    sscanf(nameFile, "%*[^/]/%n%*[^./]%n", &startIndex, &endOfIndex);
+
+    if (!(nameFile[sizeOfNameFile - 1] == '/'))
+    {
+        char *extensionFile = strrchr(nameFile, '.');
+        if (extensionFile)
+            endOfIndex = extensionFile - nameFile;
+        else
+            endOfIndex = sizeOfNameFile;
+    }
+
+    for (int currIndex = startIndex; currIndex < endOfIndex; currIndex++)
+    {
+        if (!((nameFile[currIndex] >= 0 && nameFile[currIndex] < 65) || (nameFile[currIndex] > 90 && nameFile[currIndex] < 97) || (nameFile[currIndex] > 122 && nameFile[currIndex] <= 127)))
+        {
+            if (nameFile[currIndex] >= 'A' && nameFile[currIndex] <= 'Z')
+            {
+                nameFile[currIndex] = 'Z' + 'A' - nameFile[currIndex];
+            }
+
+            if (nameFile[currIndex] >= 'a' && nameFile[currIndex] <= 'z')
+            {
+                nameFile[currIndex] = 'z' + 'a' - nameFile[currIndex];
+            }
+        }
+    }
+}
+
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
+    char *folderAtoz = strstr(path, "AtoZ_");
+    if (folderAtoz)
+        atbashCipher(folderAtoz, strlen(folderAtoz), 0);
+
     int res;
     char fpath[1000];
-
     sprintf(fpath, "%s%s", dirpath, path);
-
     res = lstat(fpath, stbuf);
 
     if (res == -1)
@@ -25,133 +61,80 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
     return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t ofdirpathet, struct fuse_file_info *fi)
 {
-    char fpath[1000];
+    char *folderAtoz = strstr(path, "AtoZ_");
+    if (folderAtoz)
+        atbashCipher(folderAtoz, strlen(folderAtoz), 0);
 
-    if (strcmp(path, "/") == 0)
+    char fpath[1000];
+    if (!strcmp(path, "/"))
     {
         path = dirpath;
         sprintf(fpath, "%s", path);
     }
     else
         sprintf(fpath, "%s%s", dirpath, path);
-
     int res = 0;
 
     DIR *dp;
     struct dirent *de;
-    (void)offset;
+    (void)ofdirpathet;
     (void)fi;
-
     dp = opendir(fpath);
-
-    if (dp == NULL)
+    if (!dp)
         return -errno;
 
-    while ((de = readdir(dp)) != NULL)
+    while ((de = readdir(dp)))
     {
         struct stat st;
-
         memset(&st, 0, sizeof(st));
-
         st.st_ino = de->d_ino;
         st.st_mode = de->d_type << 12;
-        res = (filler(buf, de->d_name, &st, 0));
 
+        int d_len = strlen(de->d_name);
+        if (folderAtoz)
+            atbashCipher(de->d_name, d_len, 1);
+        res = (filler(buf, de->d_name, &st, 0));
         if (res != 0)
             break;
     }
 
     closedir(dp);
-
     return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+static int xmp_read(const char *path, char *buf, size_t size, off_t ofdirpathet, struct fuse_file_info *fi)
 {
+    char *folderAtoz = strstr(path, "AtoZ_");
+    if (folderAtoz)
+        atbashCipher(folderAtoz, strlen(folderAtoz), 0);
+
     char fpath[1000];
-    if (strcmp(path, "/") == 0)
-    {
-        path = dirpath;
-
-        sprintf(fpath, "%s", path);
-    }
-    else
-        sprintf(fpath, "%s%s", dirpath, path);
-
-    int res = 0;
-    int fd = 0;
+    sprintf(fpath, "%s%s", dirpath, path);
+    int fd, res;
 
     (void)fi;
-
     fd = open(fpath, O_RDONLY);
-
     if (fd == -1)
         return -errno;
 
-    res = pread(fd, buf, size, offset);
-
+    res = pread(fd, buf, size, ofdirpathet);
     if (res == -1)
         res = -errno;
-
     close(fd);
-
     return res;
 }
 
-static struct fuse_operations xmp_oper = {
+static struct fuse_operations _func = {
     .getattr = xmp_getattr,
     .readdir = xmp_readdir,
     .read = xmp_read,
+
 };
-
-void atbashCipher(char *str)
-{
-    int i;
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97) || (str[i] > 122 && str[i] <= 127)))
-        {
-            if (str[i] >= 'A' && str[i] <= 'Z')
-            {
-                str[i] = 'Z' + 'A' - str[i];
-            }
-
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] = 'z' + 'a' - str[i];
-            }
-        }
-        i++;
-    }
-}
-
-char fileExtension[100000] = "\0";
-int indexStart = 0;
-
-int trimExtension(char* file)
-{
-	indexStart = 0;
-	while(indexStart < strlen(file) && file[indexStart] != '.') indexStart++;
-	memset(fileExtension, 0, sizeof(fileExtension));
-	strcpy(fileExtension, file + indexStart);
-	return indexStart;
-}
-
-void takeSubstring(char s[], char sub[], int p, int l) {
-   int c = 0;
-   while (c < l) 
-   {
-      sub[c] = s[p + c];
-      c++;
-   }
-   sub[c] = '\0';
-}
 
 int main(int argc, char *argv[])
 {
     umask(0);
-    return fuse_main(argc, argv, &xmp_oper, NULL);
+    return fuse_main(argc, argv, &_func, NULL);
 }
