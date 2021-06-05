48580730d659806dd7d407f9c29b3f47e2afd1fd
ishaqadhel <ishaq.adhel@gmail.com>
2021-06-05 11:51:11 +0700

    feat(soal1): change fuse template and add trimExtension for file function

--- SinSeiFS_C06.c
+++ SinSeiFS_C06.c
@@ -8,24 +8,43 @@
 #include <errno.h>
 #include <sys/time.h>
 
+static const char *dirpath = "/home/namdoshaq/Downloads";
+
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
     int res;
-    res = lstat(path, stbuf);
+    char fpath[1000];
+
+    sprintf(fpath, "%s%s", dirpath, path);
+
+    res = lstat(fpath, stbuf);
 
     if (res == -1)
         return -errno;
+
     return 0;
 }
 
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
+    char fpath[1000];
+
+    if (strcmp(path, "/") == 0)
+    {
+        path = dirpath;
+        sprintf(fpath, "%s", path);
+    }
+    else
+        sprintf(fpath, "%s%s", dirpath, path);
+
+    int res = 0;
+
     DIR *dp;
     struct dirent *de;
     (void)offset;
     (void)fi;
 
-    dp = opendir(path);
+    dp = opendir(fpath);
 
     if (dp == NULL)
         return -errno;
@@ -38,21 +57,35 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 
         st.st_ino = de->d_ino;
         st.st_mode = de->d_type << 12;
+        res = (filler(buf, de->d_name, &st, 0));
 
-        if (filler(buf, de->d_name, &st, 0))
+        if (res != 0)
             break;
     }
+
     closedir(dp);
+
     return 0;
 }
 
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-    int fd;
-    int res;
+    char fpath[1000];
+    if (strcmp(path, "/") == 0)
+    {
+        path = dirpath;
+
+        sprintf(fpath, "%s", path);
+    }
+    else
+        sprintf(fpath, "%s%s", dirpath, path);
+
+    int res = 0;
+    int fd = 0;
+
     (void)fi;
 
-    fd = open(path, O_RDONLY);
+    fd = open(fpath, O_RDONLY);
 
     if (fd == -1)
         return -errno;
@@ -76,7 +109,7 @@ static struct fuse_operations xmp_oper = {
 void atbashCipher(char *str)
 {
     int i;
-    i=0;
+    i = 0;
     while (str[i] != '\0')
     {
         if (!((str[i] >= 0 && str[i] < 65) || (str[i] > 90 && str[i] < 97) || (str[i] > 122 && str[i] <= 127)))
@@ -93,6 +126,18 @@ void atbashCipher(char *str)
         }
         i++;
     }
+}
+
+char fileExtension[100000] = "\0";
+int indexStart = 0;
+
+int trimExtension(char* file)
+{
+	indexStart = 0;
+	while(indexStart < strlen(file) && file[indexStart] != '.') indexStart++;
+	memset(fileExtension, 0, sizeof(fileExtension));
+	strcpy(fileExtension, file + indexStart);
+	return indexStart;
 }
 
 int main(int argc, char *argv[])
